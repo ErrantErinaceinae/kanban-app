dcc01911cb306b6ab39fb24659a27f93fe2f8fdd
Errant Erinaceinae <ErrantErinaceinae@github.com>
2016-04-22 16:44:13 -0400

    Added note deletion

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,84 +1,89 @@
 import React from 'react';
 
 export default class Note extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    // Track `editing` state.
-    this.state = {
-      editing: false
+        // Track `editing` state.
+        this.state = {
+            editing: false
+        };
+    }
+
+    render() {
+        // Render the component differently based on state.
+        if (this.state.editing) {
+            return this.renderEdit();
+        }
+
+        return this.renderNote();
+    }
+    renderEdit = () => {
+        // We deal with blur and input handlers here. These map to DOM events.
+        // We also set selection to input end using a callback at a ref.
+        // It gets triggered after the component is mounted.
+        //
+        // We could also use a string reference (i.e., `ref="input") and
+        // then refer to the element in question later in the code through
+        // `this.refs.input`. We could get the value of the input using
+        // `this.refs.input.value` through DOM in this case.
+        //
+        // Refs allow us to access the underlying DOM structure. They
+        // can be using when you need to move beyond pure React. They
+        // also tie your implementation to the browser, though.
+        return <input type="text" ref={element => element
+            ? element.selectionStart = this.props.task.length
+            : null} autoFocus={true} defaultValue={this.props.task} onBlur={this.finishEdit} onKeyPress={this.checkEnter}/>;
     };
-  }
 
-  render() {
-    // Render the component differently based on state.
-    if(this.state.editing) {
-      return this.renderEdit();
+    renderNote = () => {
+        const onDelete = this.props.onDelete;
+
+        return (
+            <div onClick={this.edit}>
+                <span>{this.props.task}</span>
+                {onDelete ? this.renderDelete() : null}
+            </div>
+        );
+    };
+
+    renderDelete = () => {
+        return <button onClick={this.props.onDelete}>Delete Note</button>;
+    };
+
+    delete = (event) => {
+        if (this.props.onDelete) {
+            this.props.onDelete(event.target.value.id);
+        }
     }
 
-    return this.renderNote();
-  }
-  renderEdit = () => {
-    // We deal with blur and input handlers here. These map to DOM events.
-    // We also set selection to input end using a callback at a ref.
-    // It gets triggered after the component is mounted.
-    //
-    // We could also use a string reference (i.e., `ref="input") and
-    // then refer to the element in question later in the code through
-    // `this.refs.input`. We could get the value of the input using
-    // `this.refs.input.value` through DOM in this case.
-    //
-    // Refs allow us to access the underlying DOM structure. They
-    // can be using when you need to move beyond pure React. They
-    // also tie your implementation to the browser, though.
-    return <input type="text"
-      ref={
-        element => element ?
-        element.selectionStart = this.props.task.length :
-        null
-      }
-      autoFocus={true}
-      defaultValue={this.props.task}
-      onBlur={this.finishEdit}
-      onKeyPress={this.checkEnter} />;
-  };
+    edit = () => {
+        // Enter edit mode.
+        this.setState({editing: true});
+    };
 
-  renderNote = () => {
-    // If the user clicks a normal note, trigger editing logic.
-    return <div onClick={this.edit}>{this.props.task}</div>;
-  };
+    checkEnter = (event) => {
+        // The user hit *enter*, let's finish up.
+        if (event.key === 'Enter') {
+            this.finishEdit(event);
+        }
+    };
 
-  edit = () => {
-    // Enter edit mode.
-    this.setState({
-      editing: true
-    });
-  };
+    finishEdit = (event) => {
+        // `Note` will trigger an optional `onEdit` callback once it
+        // has a new value. We will use this to communicate the change to
+        // `App`.
+        //
+        // A smarter way to deal with the default value would be to set
+        // it through `defaultProps`.
+        //
+        // See the *Typing event React* chapter for more information.
+        const value = event.target.value;
 
-  checkEnter = (e) => {
-    // The user hit *enter*, let's finish up.
-    if(e.key === 'Enter') {
-      this.finishEdit(e);
-    }
-  };
-  finishEdit = (e) => {
-    // `Note` will trigger an optional `onEdit` callback once it
-    // has a new value. We will use this to communicate the change to
-    // `App`.
-    //
-    // A smarter way to deal with the default value would be to set
-    // it through `defaultProps`.
-    //
-    // See the *Typing with React* chapter for more information.
-    const value = e.target.value;
-
-    if(this.props.onEdit) {
-      this.props.onEdit(value);
-
-
-    }  // Exit edit mode.
-      this.setState({
-        editing: false
-      });
-  };
+        if (this.props.onEdit) {
+            this.props.onEdit(value);
+        }
+        // Exit edit mode.
+        this.setState({editing: false});
+    };
 }
--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Note from './Note.jsx';
 
-export default ({notes}) => {
+export default({notes, onEdit,onDelete}) => {
     return (
         <ul>{notes.map(note =>
-                <li key={note.id}>
-                    <Note task={note.task} />
-                </li>
-            )}</ul>
-        );
-    }
+            <li key={note.id}>
+                <Note
+                    task={note.task}
+                    onEdit={onEdit.bind(null, note.id)}
+                    onDelete={onDelete.bind(null, note.id)}/>
+            </li>
+        )}</ul>
+    );
+}
